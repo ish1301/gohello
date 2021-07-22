1b3e98c2bd46753086015b3560f857bd6ed58dce
Ish Kumar <ish1301@gmail.com>
2021-07-22 15:40:06 -0400

    Update external modules

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module rocketsquare.com/site/gohello
 
 go 1.16
+
+require github.com/google/go-cmp v0.5.6
--- hello.go
+++ hello.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 
+	"github.com/google/go-cmp/cmp"
 	"rocketsquare.com/site/gohello/morestrings"
 )
 
 func main() {
 	fmt.Println(morestrings.ReverseRunes("!oG ,olleH"))
+	fmt.Println(cmp.Diff("Hello World", "Hello Go"))
+
 }
